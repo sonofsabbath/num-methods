28d2631005074d96f208a6892131d71cfe2d87c4
sonofsabbath <sonofsabbath@users.noreply.github.com>
2019-04-06 23:07:11 +0200

    compare_fields

--- lab3/ex1_2 (classes).py
+++ lab3/ex1_2 (classes).py
@@ -1,4 +1,4 @@
-from numpy import pi
+import numpy as np
 
 
 class Figure:
@@ -13,29 +13,59 @@ class Figure:
 
 def count_fields(fig):
     field = -1
-    try:
-        if fig == [] or (len(fig) - 1) != fig[0].parameters:
-            print('Entered too many or not enough parameters')
-        else:
-            try:
+    if (len(fig) - 1) != fig[0].parameters:
+        print('Entered too many or not enough parameters')
+    else:
+        try:
+            if sum(n <= 0 for n in fig[1:]) == 0:
                 field = fig[0].formula(fig[1:])
-            except TypeError:
-                print('Parameters must be numbers')
-    except AttributeError:
-        print('Enter name of figure first')
+            else:
+                print('Parameters must be higher than 0')
+        except TypeError:
+            print('Parameters must be numbers')
 
     return field
 
 
+def compare_fields(figs):
+    if not figs:
+        print('At least one figure is needed')
+        return -1
+
+    fields = []
+    for i in figs:
+        print(i)
+        try:
+            f_name = i[0].lower()
+            i[0] = mapping[f_name]
+
+            f = count_fields(i)
+            fields.append(f)
+            if f != -1:
+                print(f)
+        except KeyError:
+            print('Unrecognized figure')
+            fields.append(-1)
+        except AttributeError:
+            print('Enter name of figure first')
+            fields.append(-1)
+        except IndexError:
+            print('Entered empty figure data')
+            fields.append(-1)
+
+    f_max = max(fields)
+    max_figs = []
+    for i in range(len(fields)):
+        if fields[i] == f_max and fields[i] != -1:
+            max_figs.append((figs[i][0].name, i))
+
+    print('Max field:', str(f_max), '\nFigure(s) and index(es):', max_figs)
+
+
 mapping = {}
-circle = Figure('circle', 1, lambda x: pi * x[0] ** 2)
+circle = Figure('circle', 1, lambda x: np.pi * x[0] ** 2)
 rectangle = Figure('rectangle', 2, lambda x: x[0] * x[0])
 triangle = Figure('triangle', 2, lambda x: (x[0] * x[1]) / 2)
 rhombus = Figure('rhombus', 2, lambda x: (x[0] + x[1]) / 2)
 
-try:
-    f_name = 'triangle'.lower()
-    f = mapping[f_name]
-    print(count_fields([f, 3, 5]))
-except NameError:
-    print('Unrecognized figure')
+compare_fields([['rhombus', 9, 2.4], ['circle', 2.5], ['circle', 2.5], ['triangle', 'xd', 4]])
