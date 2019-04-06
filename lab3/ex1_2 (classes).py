8ee33c8618e13d032ac077983f613f38180fed0a
sonofsabbath <sonofsabbath@users.noreply.github.com>
2019-04-06 20:46:25 +0200

    count_fields
    - added field's counting
    - improved class

--- lab3/ex1_2 (classes).py
+++ lab3/ex1_2 (classes).py
@@ -2,13 +2,40 @@ from numpy import pi
 
 
 class Figure:
-    """Stores names of figures and formulas for their field"""
+    """Stores names of figures, numbers of parameters the fields' formulas takes and formulas themselves"""
 
-    def __init__(self, form):
+    def __init__(self, n, pars, form):
+        self.name = n
+        self.parameters = pars
         self.formula = form
+        mapping[self.name] = self
 
 
-circle = Figure(lambda x: pi * x ** 2)
-rectangle = Figure(lambda x, y: x * y)
-triangle = Figure(lambda x, y: (x * y) / 2)
-rhombus = Figure(lambda x, y: (x + y) / 2)
+def count_fields(fig):
+    field = -1
+    try:
+        if fig == [] or (len(fig) - 1) != fig[0].parameters:
+            print('Entered too many or not enough parameters')
+        else:
+            try:
+                field = fig[0].formula(fig[1:])
+            except TypeError:
+                print('Parameters must be numbers')
+    except AttributeError:
+        print('Enter name of figure first')
+
+    return field
+
+
+mapping = {}
+circle = Figure('circle', 1, lambda x: pi * x[0] ** 2)
+rectangle = Figure('rectangle', 2, lambda x: x[0] * x[0])
+triangle = Figure('triangle', 2, lambda x: (x[0] * x[1]) / 2)
+rhombus = Figure('rhombus', 2, lambda x: (x[0] + x[1]) / 2)
+
+try:
+    f_name = 'triangle'.lower()
+    f = mapping[f_name]
+    print(count_fields([f, 3, 5]))
+except NameError:
+    print('Unrecognized figure')
