f594b0c0795b0c0d5ff60aa62e281c0746d7bfff
sonofsabbath <sonofsabbath@users.noreply.github.com>
2019-04-07 18:11:11 +0200

    bisect_fix

--- lecture/bisection.py
+++ lecture/bisection.py
@@ -1,20 +1,45 @@
-def f(x):
-    return x ** 3.0 - x ** 2.0 + 2.0
+from numpy import abs
 
 
 def bisection(a, b, eps):
-    if f(a) * f(b) >= 0.0:
-        return 'Incorrect a, b'
-    while (b - a) >= eps:
-        c = (a + b) / 2.0
-        if f(c) == 0.0:
+    if f(a) * f(b) >= 0:
+        print('Incorrect a, b')
+    while abs(b - a) >= eps:
+        c = (a + b) / 2
+        if f(c) == 0:
             return c
-        elif f(a) * f(c) < 0.0:
+        elif f(a) * f(c) < 0:
             b = c
         else:
             a = c
-        print(c)
-    return c
+    print(c)
 
 
-bisection(-200.0, 100.0, 0.00001)
+def bisect_fix(a, b, eps, sig):
+    if sign(f(a)) == sign(f(b)):  # uniknięcie niedomiaru/nadmiaru ze zbędnego mnożenia
+        print('Incorrect a, b')
+    c = (a + b) / 2
+    while abs(b - a) >= eps or f(c) > sig:  # kończymy gdy błąd jest dostacznie mały i f(c) jest bardzo bliskie zeru
+        c = a + (b - a) / 2   # lepiej dodać coś do poprzedniej wielkości; może się zdarzyć, że c wyjdzie poza [a,b]
+        if f(c) == 0:
+            return c
+        elif sign(f(a)) != sign(f(c)):
+            b = c
+        else:
+            a = c
+    print(c)
+
+
+def f(x):
+    return x ** 3 - x ** 2 + 2
+
+
+def sign(x):
+    if x >= 0:
+        return 1
+    else:
+        return 0
+
+
+bisection(-200, 100, 0.0001)
+bisect_fix(-200, 100, 0.0001, 0.00000000001)
