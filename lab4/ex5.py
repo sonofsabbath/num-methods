3a220832b9e6b00a65ea4b631febcc65cddc4934
sonofsabbath <sonofsabbath@users.noreply.github.com>
2019-04-21 20:36:46 +0200

    global optimum

--- lab4/ex5.py
+++ lab4/ex5.py
@@ -7,10 +7,10 @@ def f(x):
     return 100 * np.sqrt(np.abs(x[1] - 0.01 * x[0] ** 2)) + 0.01 * np.abs(x[0] + 10)  # Bukin function No. 6
 
 
-def gradient_descent(x, alpha, eta, epochs, prev_init, val_init):
+def gradient_descent(x, alpha, eta, epochs):
     e = 0
-    f_prev = prev_init
-    f_val = val_init
+    f_prev = 100
+    f_val = 0
 
     while e < epochs and np.abs(f_prev - f_val) >= eta:
         f_prev = f_val
@@ -25,15 +25,27 @@ def gradient_descent(x, alpha, eta, epochs, prev_init, val_init):
     return x, f_val
 
 
+def global_optimum():
+    xs = []
+    vals = []
+
+    for i in range(-15, 6):
+        for j in range(-3, 4):
+            x12, value = gradient_descent([i, j], 0.0001, 0.0001, 10000)
+            if not np.isnan(value):
+                xs.append(x12)
+                vals.append(value)
+
+    ind = int(np.argmin(vals))
+    return xs[ind], vals[ind]
+
+
 x1 = np.linspace(-15, 5, 50)
 x2 = np.linspace(-3, 3, 50)
-x_init = [-11.0, 1.0]
-prev = f(x_init)
-val = 0.0
 df = np.zeros(2)
 X, Y = np.meshgrid(x1, x2)
-x12, value = gradient_descent(x_init, 0.0001, 0.0001, 10000, prev, val)
-print(x12, value)
+res, val = global_optimum()
+print(res, val)
 
 Z = np.zeros(X.shape)
 for i in range(Z.shape[0]):
@@ -42,5 +54,5 @@ for i in range(Z.shape[0]):
 
 ax = Axes3D(plt.figure())
 ax.plot_surface(X, Y, Z)
-ax.plot([x12[0]], [x12[1]], [value], marker='o', markersize=5)
+ax.plot([res[0]], [res[1]], [val], marker='o', markersize=5)
 plt.show()
